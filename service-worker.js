6e4b4c4f0c415f06fb615819ffcb28cac9d8eb1a
pilazeleznice-coder <pilazeleznice@gmail.com>
2026-08-31 23:04:33 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,47 +1,57 @@
-const CACHE_NAME = "konfigurator-reziva-v1";
+const CACHE_NAME = "konfigurator-reziva-v2";
 
 const APP_FILES = [
   "./",
   "./index.html",
-  "./manifest.webmanifest"
+  "./manifest.webmanifest",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
 ];
 
-self.addEventListener("install", event => {
+self.addEventListener("install", (event) => {
   self.skipWaiting();
 
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(APP_FILES))
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(APP_FILES);
+    })
   );
 });
 
-self.addEventListener("activate", event => {
+self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.keys().then(cacheNames =>
-      Promise.all(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
         cacheNames
-          .filter(name => name !== CACHE_NAME)
-          .map(name => caches.delete(name))
-      )
-    )
+          .filter((name) => name !== CACHE_NAME)
+          .map((name) => caches.delete(name))
+      );
+    }).then(() => self.clients.claim())
   );
-
-  self.clients.claim();
 });
 
-self.addEventListener("fetch", event => {
+self.addEventListener("fetch", (event) => {
   if (event.request.method !== "GET") return;
+
+  const url = new URL(event.request.url);
+
+  if (url.origin !== self.location.origin) return;
 
   event.respondWith(
     fetch(event.request)
-      .then(response => {
-        const copy = response.clone();
+      .then((response) => {
+        if (response && response.status === 200) {
+          const copy = response.clone();
 
-        caches.open(CACHE_NAME)
-          .then(cache => cache.put(event.request, copy));
+          caches.open(CACHE_NAME).then((cache) => {
+            cache.put(event.request, copy);
+          });
+        }
 
         return response;
       })
-      .catch(() => caches.match(event.request))
+      .catch(() => {
+        return caches.match(event.request);
+      })
   );
 });
